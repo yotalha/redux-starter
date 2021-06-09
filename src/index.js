2f809e8ec143199f866ae1f8d17c5a94c18079d3
yotalha <talha.coder9@gmail.com>
2021-06-09 06:13:01 +0500

    mosh 1 hour video about redux completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import store from './store';
+import {bugAdded, bugResolved} from './actions';
+
+
+
+
+store.dispatch(bugAdded('Bug1'));
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());  
+
